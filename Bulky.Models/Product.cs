c40d7a9deab015921f1ecbdfc6f192d12b32f0bc
Bronwyn Potgieter <bronwynpottie@gmail.com>
2024-04-24 22:05:09 +0200

    Created Product Model

---
+++ Bulky.Models/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+	public class Product
+	{
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        [Required]
+        public required string ISBN { get; set; }
+
+        [Required]
+        public required string Author { get; set; }
+
+        [Required]
+        [DisplayName("List Price")]
+        [Range(1, 1000)]
+        public required double ListPrice { get; set; }
+
+        [Required]
+        [DisplayName("Price for 1-10")]
+        [Range(1, 1000)]
+        public required double Price { get; set; }
+
+        [Required]
+        [DisplayName("Price for 50+")]
+        [Range(1, 1000)]
+        public required double Price50 { get; set; }
+
+        [Required]
+        [DisplayName("Price for 100+")]
+        [Range(1, 1000)]
+        public required double Price100 { get; set; }
+    }
+}
+
